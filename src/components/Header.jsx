6d2d263793514909dca85383ff0ae02395934fdc
esma <esma.ea.aksoy@gmail.com>
2023-12-09 02:09:17 +0300

    end

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { Container, Image } from "react-bootstrap"
-import logo from "../assets/nba-logo.png"
+import logo from "../assets/corolla-4785884_1280.webp"
 const Header = ()=>{
 return(
-    <Container>
-        <Image src={logo}></Image>
-        <h1 className="my-2 font-monospace display-4 fw-bold">NBA Legends</h1>
+    <Container className="header">
+        <Image src={logo} className="logo"></Image>
+        <span className="leaf leaf1">🍃</span>
+        <h1 className="my-2 font-monospace display-4 fw-bold">Cyrano de Bergerac </h1>
     </Container>
 )
 }
